c09165e97c9ec4b102154f78229cd3f0fd2cac90
mottej2269 <160337521+mottej2269@users.noreply.github.com>
2024-02-17 10:51:05 -0500

    Add files via upload

---
+++ P1LAB1_Motte_Jessica.py
@@ -0,0 +1,8 @@
+Python 3.12.1 (v3.12.1:2305ca5144, Dec  7 2023, 17:23:39) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: /Users/jessicamotte/Desktop/python.py ================
+Jessica Motte
+February 17, 2024
+P1Lab1
+Introduction to using Python
