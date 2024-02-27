35df5a0cf9bfbe15536539e1ba35b3a266a012ee
mottej2269 <160337521+mottej2269@users.noreply.github.com>
2024-02-27 00:02:53 -0500

    Add files via upload

---
+++ P1HW2_MotteJessica.py
@@ -0,0 +1,17 @@
+"""
+Jessica Motte
+Feb. 25, 2024
+P1HW2
+'Travel Expenses'
+"""
+
+
+print('This program calculates and displays travel expenses')
+
+budget = int(input('Enter integer:\n'))
+gas = int(input('Enter another integer:\n'))
+hotel = int(input('Enter another integer:\n'))
+food = int(input('Enter another integer:\n'))
+remaining_bud = int(input('Enter another integer:\n'))
+print('Location')
+print(budget,"-",gas,"-",hotel,"-",food,"=",remaining_bud)
